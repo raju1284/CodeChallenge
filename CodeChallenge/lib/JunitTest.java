443e92ecb13ffdbfa724a054ad7f074efad2505e
raju1284 <56700079+raju1284@users.noreply.github.com>
2021-11-16 14:27:09 -0600

    Files Updated

---
+++ CodeChallenge/lib/JunitTest.java
@@ -0,0 +1,93 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+public class JunitTest {
+	
+// Initaile the TransposeLongestWord  class	
+TransposeLongestWord TLword = new TransposeLongestWord();
+String TestDataFilesPath ="C:\\Users\\"+System.getenv("USERNAME")+"\\Desktop\\CodeChallenge\\TestDataFiles\\";
+	
+//Test1- Longest Words is at the begining of the file
+	@Test
+	public void testvalidfile_first() throws FileNotFoundException {
+	System.out.println("Valid File - Longest word at the beginning file");
+	String logestword = TransposeLongestWord.longestWord(TestDataFilesPath+"Test1.txt");
+	String transposeword =TransposeLongestWord.TransposeWord(logestword);
+        assertEquals("abcde",logestword);
+	assertEquals("edcba",transposeword);
+	
+	}
+//Test2- Longest Word is at the middle of the file
+	@Test
+	public void testvalidfile_middle() throws FileNotFoundException {
+	System.out.println("Valid File - Longest word at the middle of file");
+	String logestword = TransposeLongestWord.longestWord(TestDataFilesPath+"Test2.txt");
+	String transposeword =TransposeLongestWord.TransposeWord(logestword);
+	assertEquals("abcde",logestword);	
+	assertEquals("edcba",transposeword);
+	}
+
+//Test3- Longest Word is at the End of the file
+	@Test
+	public void testvalidfile_last() throws FileNotFoundException {
+	System.out.println("Valid File - Longest word at the endfile");
+	String logestword = TransposeLongestWord.longestWord(TestDataFilesPath+"Test3.txt");
+	String transposeword =TransposeLongestWord.TransposeWord(logestword);
+	assertEquals("abcde",logestword);
+	assertEquals("edcba",transposeword);
+	}
+
+//Test4- Longest Word is in numeric format
+	@Test
+	public void testvalidfile_numbersonlyword() throws FileNotFoundException {
+	System.out.println("Valid File - Longest word is numbers only");
+	String logestword = TransposeLongestWord.longestWord(TestDataFilesPath +"Test4.txt");
+	String transposeword =TransposeLongestWord.TransposeWord(logestword);
+	assertEquals("12345",logestword);
+	assertEquals("54321",transposeword);
+	}
+//Test5- Longest Word is in AlphaNumeric format
+	@Test
+	public void testvalidfile_charsnumswords() throws FileNotFoundException {
+	System.out.println("Valid File - Longest word is alphanumeric");
+	String logestword = TransposeLongestWord.longestWord(TestDataFilesPath +"Test5.txt");
+	String transposeword =TransposeLongestWord.TransposeWord(logestword);
+	assertEquals("abc123",logestword);
+	assertEquals("321cba",transposeword);
+	}
+//Test6- Longest Word is in with special chars format
+	@Test
+	public void testvalidfile_specialcharsword() throws FileNotFoundException {
+	System.out.println("Valid File - Longest word is included with special chars");
+	String logestword = TransposeLongestWord.longestWord(TestDataFilesPath +"Test6.txt");
+	String transposeword =TransposeLongestWord.TransposeWord(logestword);
+	assertEquals("abcde$%",logestword);	
+	assertEquals("%$edcba",transposeword);
+	}
+	
+//Test7- Invalid File type	
+	@Test
+	public void testInvalidfileformat() throws FileNotFoundException {	
+		System.out.println("Incorrect File extension");
+		String logestword = TransposeLongestWord.longestWord(TestDataFilesPath +"Test1.xls");
+		assertEquals("Please enter the path in text file format only",logestword);
+	}
+//Test8- Given File not found
+	@Test
+	public void testfilenotfound() throws FileNotFoundException {	
+		System.out.println("File not found");
+		String logestword = TransposeLongestWord.longestWord(TestDataFilesPath +"Test9.txt");
+		assertEquals("File not Found",logestword);
+	}
+//Test9- Empty File No Data or Spaces
+	@Test
+	public void testEmptyfile() throws FileNotFoundException {	
+		System.out.println("Empty File");
+		String logestword = TransposeLongestWord.longestWord(TestDataFilesPath +"Emptyfile.txt");
+		assertEquals("Emptyfile No words",logestword);
+	}
+	
+
+}
---
+++ CodeChallenge/lib/TransposeLongestWord.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class TransposeLongestWord {
+
+	// Method to read the input file and find the longest word and return the word
+	public static String longestWord(String Filepath) throws FileNotFoundException
+	{
+		String longest_word = "";
+	       String current;
+	       //Checking the file format
+	       if (Filepath.endsWith(".txt"))
+			{
+	    	   try {
+	    		   	//Reading the file to scanner class
+				Scanner sc = new Scanner(new File(Filepath));
+
+	    		   	//checking the longest word
+				while (sc.hasNext()) {
+	    		   	current = sc.next();
+	    		   		if (current.length() > longest_word.length()) {
+	    		   			longest_word = current;
+	             
+	    		   		}
+
+	    		   	}
+	    	   }
+		//Handling the File not found exception
+	    	   catch(Exception FileNotFoundException)
+	    	   {
+	    	   longest_word = "File not Found";
+	    	   }
+			}
+	       else
+	    	   longest_word = "Please enter the path in text file format only";
+	       if (longest_word=="")
+	    	   longest_word="Emptyfile No words";
+	      
+	       return longest_word;
+	}
+//Method to reverse a string and return the string using StringBuilder reverse function
+	public static String TransposeWord(String longestword)
+	{
+       
+         
+	         StringBuilder Tlongest_word = new StringBuilder();
+	         Tlongest_word.append(longestword);
+	         Tlongest_word.reverse();         
+	         return Tlongest_word.toString();
+    }
+// Main method to test the above methods	
+	public static void main(String [] args) throws FileNotFoundException
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the full path of file with file name and its extension:");
+		String Filenamepath = sc.nextLine(); // Reading the input file path from the user
+		
+			String logestword = longestWord(Filenamepath); // finding the longest word
+			
+			if (logestword=="File not Found" || logestword=="Please enter the path in text file format only")
+				System.out.println(logestword);
+			else
+				
+			{
+				System.out.println("Longesword in a file:"+logestword);
+				String transposeword =TransposeWord(logestword); //Reverse  the string
+				System.out.println("Transposed word:"+transposeword);
+				}
+		sc.close();		
+			
+	}
+	
+}
